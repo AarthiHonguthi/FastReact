07fb6cbc0c175932441ed3da5a9fda3d82915a75
unknown <aarthihonguthi025@gmail.com>
2024-04-02 09:39:13 +0530

    Updated code in <04bgChanger>

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -4,12 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
+
+  // function changeColor(color) {
+  //   setColor(color)
+  // }
 
   return (
-    <>
-      <h1 className = 'bg-green-500 text-3xl'>A bg changer app with Vite</h1>
-    </>
+    <div className='w-full h-screen duration-200 ' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+          onClick={() => setColor('red')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor: 'red'}}
+          >Red</button>
+          <button
+          onClick={() => setColor('green')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor: 'green'}}
+          >Green</button>
+          
+          </div>
+      </div>
+    </div>
   )
 }
 
